239417425ffae1f009e270cc43d43b919bb47122
mingming0324 <alsehfdl14@gmail.com>
2025-09-24 18:27:54 +0900

    sec-2 제작중

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+//main
 var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
     loop: true,
-//     pagination: {
-// el: ".swiper-pagination",
-//     clickable: true,
-//     },
+});
+
+//sec-1
+var swiper = new Swiper(".mySwiper-1", {
+    direction: "vertical",
+    loop: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
 });
